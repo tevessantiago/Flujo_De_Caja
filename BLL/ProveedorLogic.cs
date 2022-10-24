07c454d2f930c0277b33e273c219f65347b31d9e
emmacamblor <55325325+emmacamblor@users.noreply.github.com>
2022-10-23 21:00:51 -0300

    Ajuste de ProveedorDao

--- BLL/ProveedorLogic.cs
+++ BLL/ProveedorLogic.cs
@@ -1,7 +1,20 @@
-﻿namespace BLL
+﻿using DAL;
+using Entidades;
+namespace BLL
 {
     public class ProveedorLogic
     {
+        ProveedorDao dao = new ProveedorDao();
+
+        public void CargarProveedor(Proveedor proveedor)
+        {
+            dao.CargarProveedor(proveedor);
+        }
+
+        public List<Proveedor> ObtenerProveedores()
+        { 
+            return dao.ObtenerProveedores();
+        }
 
     }
 }
